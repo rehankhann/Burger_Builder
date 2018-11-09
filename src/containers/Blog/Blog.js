61542c1a0839e5c18b88263f219e3c78ec34ce1b
Muqeem khan <muqeem.khan@icreon.com>
2018-11-09 16:22:32 +0530

    add post from api

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,35 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        post : []
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            /* Transform Data */
+            const slicepost = response.data.slice(0,4);
+            const Updatedauthor = slicepost.map(oldpost => {
+                return{
+                    ...oldpost,
+                    author:'khan'
+                }
+            });
+            this.setState({post:Updatedauthor})
+        });
+    }
+
     render () {
+
+        const postdetails = this.state.post.map(FetchPost => {
+            return <Post key={FetchPost.id} Title={FetchPost.title} author={FetchPost.author} />
+        }); 
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                {postdetails}
                 </section>
                 <section>
                     <FullPost />
